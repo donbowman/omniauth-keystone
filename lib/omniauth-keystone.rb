9aeec73b6358c8230b7e6031466d97d0b2f55690
don <don.waterloo@gmail.com>
2014-10-05 16:43:09 -0400

    kinda working

--- lib/omniauth-keystone.rb
+++ lib/omniauth-keystone.rb
@@ -8,56 +8,61 @@ module OmniAuth
     class Keystone
       include OmniAuth::Strategy
 
-      args [:api_token]
+      #args [:auth_url]
 
       option :auth_url, nil
       option :name, 'keystone'
-      option :app_id, nil
-      option :app_secret, nil
+      option :setup, false
+      option :skip_info, true
 
-      attr_accessor :auth_token
+
+      def log(level, message)
+        OmniAuth.logger.send(level, "(#{name}) #{message}")
+      end
 
       def request_phase
-        OmniAuth::Form.build(
-          :title => (options[:title] || "Authenticate"), 
-          :url => callback_path
-        ) do |field|
-          field.text_field 'Username', 'username'
-          field.password_field 'Password', 'password'
-        end.to_response
+        log :info, "--Request phase initiated."
+        f = OmniAuth::Form.new(:title => (options[:title] || "OpenStack Authentication"), :url => callback_path)
+        f.text_field 'Login', 'username'
+        f.password_field 'Password', 'password'
+        f.button "Make it so"
+        f.to_response
       end
 
       def callback_phase
-        os = OpenStack::Connection.create({:username => request['username'],
-                                           :api_key=>request['password'],
-                                           :auth_method=>"password",
-                                           :auth_url => auth_url,
-                                           :authtenant_name => request['username']})
-        super
-      end
-
-      def callback_phase
-        rpam_opts = Hash.new
-        rpam_opts[:service] = options[:service] unless options[:service].nil?
-
-        unless Rpam.auth(request['username'], request['password'], rpam_opts)
-          return fail!(:invalid_credentials)
+        log :info, "--Callback phase initiated."
+        begin
+          log :info, "--Callback phase (1)."
+          log :info, "auth_url: %s" % [options.auth_url]
+          log :info, "args: %s" % [options]
+          os = OpenStack::Connection.create({:username => request['username'],
+                                             :api_key=>request['password'],
+                                             :auth_method=>"password",
+                                             :auth_url => options.auth_url,
+                                             :authtenant_name => request['username']})
+          log :info, "--Callback phase (2)."
+        rescue Exception => e
+          log :info, "--Callback phase (3)."
+          log :info, e.message
+          return fail!(:keystone_error, e)
         end
-
-        super
-      end
-
-      uid do
-        raw_info['user']['user_id']
-      end
-
-      info do
-        {
-          :name     => raw_info['user']['uid'],
-          :nickname => raw_info['user']['uid'],
-          :email    => raw_info['user']['email'],
-          :nickname => raw_info['user']['login'],
+        log :info, "--Callback phase (4)."
+	@user = request['username']
+	@user_email = request['username']
+        log :info, "--Callback phase (5)."
+        @user_info = {
+            'name' => @user
         }
+        log :info, "--Callback phase (6) %s" % [@user]
+      end
+
+      uid do 
+	@user
+      end
+      info do {
+        :name => @user,
+        :email => @user_email
+      }
       end
     end
   end
